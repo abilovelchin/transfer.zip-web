7991fce5ed9cb37cbdf330454ddb631bfd39914b
Elchin Abilov <abilovelchin@gmail.com>
2025-05-05 21:18:28 +0400

    package.json və komponent fayllarında kod formatı düzəlişləri edildi; import ifadələri və JSX elementləri arasında boşluqlar əlavə olundu, kodun oxunaqlılığı artırıldı.

--- frontend/src/routes/quick-share/QuickShareNew.js
+++ frontend/src/routes/quick-share/QuickShareNew.js
@@ -4,69 +4,78 @@ import { useContext, useEffect, useState } from "react";
 import { QuickShareContext } from "../QuickSharePage";
 import BIcon from "../../components/BIcon";
 
-export default function QuickShareNew({ }) {
-  const { hasBeenSentLink, k, remoteSessionId, transferDirection } = useContext(QuickShareContext)
+export default function QuickShareNew({}) {
+  const { hasBeenSentLink, k, remoteSessionId, transferDirection } =
+    useContext(QuickShareContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleFiles = (files) => {
-
     if (hasBeenSentLink) {
       navigate("progress", {
         state: {
           files,
           // These fields are prepopulated from link
-          k, remoteSessionId, transferDirection
-        }
-      })
-    }
-    else {
+          k,
+          remoteSessionId,
+          transferDirection,
+        },
+      });
+    } else {
       navigate("progress", {
         state: {
           files,
-          transferDirection: "S"
-        }
-      })
+          transferDirection: "S",
+        },
+      });
     }
-  }
+  };
 
-  const onReceiveClicked = e => {
+  const onReceiveClicked = (e) => {
     navigate("progress", {
       state: {
-        transferDirection: "R"
-      }
-    })
-  }
+        transferDirection: "R",
+      },
+    });
+  };
 
-  const [stars, setStars] = useState(null)
+  const [stars, setStars] = useState(null);
 
   useEffect(() => {
     fetch("https://api.github.com/repos/robinkarlberg/transfer.zip-web", {
-      "credentials": "omit",
-      "method": "GET"
-    }).then(res => res.json()).then(json => {
-      setStars(json.stargazers_count)
-    }).catch(err => {
-      console.log("GitHub stars fetch error :(")
+      credentials: "omit",
+      method: "GET",
     })
-  }, [])
+      .then((res) => res.json())
+      .then((json) => {
+        setStars(json.stargazers_count);
+      })
+      .catch((err) => {
+        console.log("GitHub stars fetch error :(");
+      });
+  }, []);
 
   return (
     <div className="w-full max-w-96 text-center">
       <div className={hasBeenSentLink ? "mb-2" : "mb-28"}>
-        <h1 className="font-bold text-4xl md:text-5xl mb-2">{hasBeenSentLink ? "Send Files" : "Quick Share"}</h1>
+        <h1 className="font-bold text-4xl md:text-5xl mb-2">
+          {hasBeenSentLink ? "Send Files" : "Quick Share"}
+        </h1>
         <h2 className="text-gray-800 mb-4 md:text-lg">
-          {hasBeenSentLink ?
-            "Someone has requested you to send files!"
-            :
-            "Send files in realtime, with no size limit."
-          }
+          {hasBeenSentLink
+            ? "Someone has requested you to send files!"
+            : "Send files in realtime, with no size limit."}
         </h2>
       </div>
-      <FileUpload onFiles={handleFiles} onReceiveClicked={hasBeenSentLink ? undefined : onReceiveClicked} />
+      <FileUpload
+        onFiles={handleFiles}
+        onReceiveClicked={hasBeenSentLink ? undefined : onReceiveClicked}
+      />
       <p className="text-gray-500 text-xs mt-2">
-        We do not use cookies. Your files are protected with end-to-end encryption, meaning they remain unreadable by anyone but you.<br /><a href="https://github.com/robinkarlberg/transfer.zip-web" className="text-primary hover:underline">GitHub {stars && <span>({stars} <BIcon name={"star"}/>)</span>} </a>
+        We do not use cookies. Your files are protected with end-to-end
+        encryption, meaning they remain unreadable by anyone but you.
+        <br />
       </p>
     </div>
-  )
+  );
 }
